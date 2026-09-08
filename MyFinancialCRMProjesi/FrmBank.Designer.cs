35c212318310838800f33ff519f49bffec732925
Serkan <serkankabadayiogullari8233@gmail.com>
2026-09-08 18:12:27 +0300

    Banka formu adında başlık olarak bir label ve ana başlık alanı için bir adet panel eklendi

--- MyFinancialCRMProjesi/FrmBank.Designer.cs
+++ MyFinancialCRMProjesi/FrmBank.Designer.cs
@@ -37,7 +37,10 @@
 			this.button6 = new System.Windows.Forms.Button();
 			this.button7 = new System.Windows.Forms.Button();
 			this.button8 = new System.Windows.Forms.Button();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
 			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// panel1
@@ -51,7 +54,7 @@
 			this.panel1.Controls.Add(this.button3);
 			this.panel1.Controls.Add(this.button2);
 			this.panel1.Controls.Add(this.button1);
-			this.panel1.Location = new System.Drawing.Point(2, 47);
+			this.panel1.Location = new System.Drawing.Point(3, 41);
 			this.panel1.Name = "panel1";
 			this.panel1.Size = new System.Drawing.Size(200, 450);
 			this.panel1.TabIndex = 0;
@@ -62,7 +65,7 @@
 			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.button1.Location = new System.Drawing.Point(23, 17);
+			this.button1.Location = new System.Drawing.Point(21, 11);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(174, 31);
 			this.button1.TabIndex = 1;
@@ -75,7 +78,7 @@
 			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.button2.Location = new System.Drawing.Point(23, 54);
+			this.button2.Location = new System.Drawing.Point(21, 48);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(174, 31);
 			this.button2.TabIndex = 2;
@@ -88,7 +91,7 @@
 			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.button3.Location = new System.Drawing.Point(23, 91);
+			this.button3.Location = new System.Drawing.Point(21, 85);
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(174, 31);
 			this.button3.TabIndex = 3;
@@ -101,7 +104,7 @@
 			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.button4.Location = new System.Drawing.Point(23, 128);
+			this.button4.Location = new System.Drawing.Point(21, 122);
 			this.button4.Name = "button4";
 			this.button4.Size = new System.Drawing.Size(174, 31);
 			this.button4.TabIndex = 4;
@@ -114,7 +117,7 @@
 			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button5.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.button5.Location = new System.Drawing.Point(23, 165);
+			this.button5.Location = new System.Drawing.Point(21, 159);
 			this.button5.Name = "button5";
 			this.button5.Size = new System.Drawing.Size(174, 31);
 			this.button5.TabIndex = 5;
@@ -127,7 +130,7 @@
 			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.button6.Location = new System.Drawing.Point(23, 202);
+			this.button6.Location = new System.Drawing.Point(21, 196);
 			this.button6.Name = "button6";
 			this.button6.Size = new System.Drawing.Size(174, 31);
 			this.button6.TabIndex = 6;
@@ -140,7 +143,7 @@
 			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button7.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.button7.Location = new System.Drawing.Point(23, 239);
+			this.button7.Location = new System.Drawing.Point(21, 233);
 			this.button7.Name = "button7";
 			this.button7.Size = new System.Drawing.Size(174, 31);
 			this.button7.TabIndex = 7;
@@ -153,23 +156,47 @@
 			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button8.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.button8.Location = new System.Drawing.Point(23, 276);
+			this.button8.Location = new System.Drawing.Point(21, 270);
 			this.button8.Name = "button8";
 			this.button8.Size = new System.Drawing.Size(174, 31);
 			this.button8.TabIndex = 8;
 			this.button8.Text = "Çıkış Yap";
 			this.button8.UseVisualStyleBackColor = false;
 			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Location = new System.Drawing.Point(3, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(792, 37);
+			this.panel2.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label1.Location = new System.Drawing.Point(3, 7);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(108, 19);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Banka Formu";
+			// 
 			// FrmBank
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
 			this.ClientSize = new System.Drawing.Size(800, 555);
+			this.Controls.Add(this.panel2);
 			this.Controls.Add(this.panel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "FrmBank";
 			this.Text = "FrmBank";
 			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -185,5 +212,7 @@
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button8;
 		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
 	}
 }
