27626ae68365c873efeb40ed681bf7599aa05396
Serkan <serkankabadayiogullari8233@gmail.com>
2026-09-09 17:59:41 +0300

    Ödemeler Form Arayüzü oluşturuldu

---
+++ MyFinancialCRMProjesi/FrmBilling.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace MyFinancialCRMProjesi
+{
+	partial class FrmBilling
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.btnListBilling = new System.Windows.Forms.Button();
+			this.btnNewBilling = new System.Windows.Forms.Button();
+			this.btnDeleteBilling = new System.Windows.Forms.Button();
+			this.btnUpdateBilling = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Gray;
+			this.panel1.Controls.Add(this.button8);
+			this.panel1.Controls.Add(this.button7);
+			this.panel1.Controls.Add(this.button6);
+			this.panel1.Controls.Add(this.button5);
+			this.panel1.Controls.Add(this.button4);
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(1, 2);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 450);
+			this.panel1.TabIndex = 1;
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.Transparent;
+			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button8.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button8.Location = new System.Drawing.Point(21, 270);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(174, 31);
+			this.button8.TabIndex = 8;
+			this.button8.Text = "Çıkış Yap";
+			this.button8.UseVisualStyleBackColor = false;
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.Transparent;
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button7.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button7.Location = new System.Drawing.Point(21, 233);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(174, 31);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "Ayarlar";
+			this.button7.UseVisualStyleBackColor = false;
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.Transparent;
+			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button6.Location = new System.Drawing.Point(21, 196);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(174, 31);
+			this.button6.TabIndex = 6;
+			this.button6.Text = "Dashboard";
+			this.button6.UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.Transparent;
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button5.Location = new System.Drawing.Point(21, 159);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(174, 31);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "Banka Hareketleri";
+			this.button5.UseVisualStyleBackColor = false;
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Transparent;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button4.Location = new System.Drawing.Point(21, 122);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(174, 31);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "Giderler";
+			this.button4.UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Transparent;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button3.Location = new System.Drawing.Point(21, 85);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(174, 31);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Faturalar";
+			this.button3.UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Transparent;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button2.Location = new System.Drawing.Point(21, 48);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(174, 31);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Bankalar";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button1.Location = new System.Drawing.Point(21, 11);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(174, 31);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Kategoriler";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.panel2.Controls.Add(this.textBox4);
+			this.panel2.Controls.Add(this.label4);
+			this.panel2.Controls.Add(this.textBox3);
+			this.panel2.Controls.Add(this.label3);
+			this.panel2.Controls.Add(this.textBox2);
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Controls.Add(this.textBox1);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Controls.Add(this.btnUpdateBilling);
+			this.panel2.Controls.Add(this.btnDeleteBilling);
+			this.panel2.Controls.Add(this.btnNewBilling);
+			this.panel2.Controls.Add(this.btnListBilling);
+			this.panel2.Location = new System.Drawing.Point(202, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(679, 211);
+			this.panel2.TabIndex = 2;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.panel3.Controls.Add(this.dataGridView1);
+			this.panel3.Location = new System.Drawing.Point(202, 229);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(679, 223);
+			this.panel3.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label1.Location = new System.Drawing.Point(0, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(84, 19);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Ödeme Id:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(89, 18);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(570, 24);
+			this.textBox1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(89, 55);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(570, 24);
+			this.textBox2.TabIndex = 6;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label2.Location = new System.Drawing.Point(26, 60);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 19);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Başlık:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(89, 92);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(570, 24);
+			this.textBox3.TabIndex = 8;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label3.Location = new System.Drawing.Point(21, 97);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(63, 19);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Miktar:";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(89, 129);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(570, 24);
+			this.textBox4.TabIndex = 10;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label4.Location = new System.Drawing.Point(22, 134);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(62, 19);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Periyot";
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(679, 223);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// btnListBilling
+			// 
+			this.btnListBilling.BackColor = System.Drawing.Color.Transparent;
+			this.btnListBilling.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnListBilling.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnListBilling.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnListBilling.Location = new System.Drawing.Point(89, 168);
+			this.btnListBilling.Name = "btnListBilling";
+			this.btnListBilling.Size = new System.Drawing.Size(132, 31);
+			this.btnListBilling.TabIndex = 11;
+			this.btnListBilling.Text = "Ödeme Listesi";
+			this.btnListBilling.UseVisualStyleBackColor = false;
+			// 
+			// btnNewBilling
+			// 
+			this.btnNewBilling.BackColor = System.Drawing.Color.Transparent;
+			this.btnNewBilling.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnNewBilling.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnNewBilling.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnNewBilling.Location = new System.Drawing.Point(227, 168);
+			this.btnNewBilling.Name = "btnNewBilling";
+			this.btnNewBilling.Size = new System.Drawing.Size(134, 31);
+			this.btnNewBilling.TabIndex = 12;
+			this.btnNewBilling.Text = "Yeni Ödeme";
+			this.btnNewBilling.UseVisualStyleBackColor = false;
+			// 
+			// btnDeleteBilling
+			// 
+			this.btnDeleteBilling.BackColor = System.Drawing.Color.Transparent;
+			this.btnDeleteBilling.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnDeleteBilling.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDeleteBilling.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnDeleteBilling.Location = new System.Drawing.Point(367, 168);
+			this.btnDeleteBilling.Name = "btnDeleteBilling";
+			this.btnDeleteBilling.Size = new System.Drawing.Size(132, 31);
+			this.btnDeleteBilling.TabIndex = 13;
+			this.btnDeleteBilling.Text = "Ödeme Sil";
+			this.btnDeleteBilling.UseVisualStyleBackColor = false;
+			// 
+			// btnUpdateBilling
+			// 
+			this.btnUpdateBilling.BackColor = System.Drawing.Color.Transparent;
+			this.btnUpdateBilling.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnUpdateBilling.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnUpdateBilling.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnUpdateBilling.Location = new System.Drawing.Point(505, 168);
+			this.btnUpdateBilling.Name = "btnUpdateBilling";
+			this.btnUpdateBilling.Size = new System.Drawing.Size(154, 31);
+			this.btnUpdateBilling.TabIndex = 14;
+			this.btnUpdateBilling.Text = "Ödeme Güncelle";
+			this.btnUpdateBilling.UseVisualStyleBackColor = false;
+			// 
+			// FrmBilling
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.ClientSize = new System.Drawing.Size(882, 453);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "FrmBilling";
+			this.Text = "FrmBilling";
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button btnUpdateBilling;
+		private System.Windows.Forms.Button btnDeleteBilling;
+		private System.Windows.Forms.Button btnNewBilling;
+		private System.Windows.Forms.Button btnListBilling;
+	}
+}
