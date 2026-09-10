6e2894fe4eb08315f14dc815c8b3d26dcd8e092d
Serkan <serkankabadayiogullari8233@gmail.com>
2026-09-10 17:11:52 +0300

    Dashboard sayfası tasarlandı, toplam bakiye, Fatura başlığı ve Gelen son havale işelmleri arayüze yansıtıldı

--- MyFinancialCRMProjesi/FrmDashboard.Designer.cs
+++ MyFinancialCRMProjesi/FrmDashboard.Designer.cs
@@ -29,12 +29,325 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.lblBankProcessAmout = new System.Windows.Forms.Label();
+			this.lblVakif = new System.Windows.Forms.Label();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.lblBillAmout = new System.Windows.Forms.Label();
+			this.lblBillTitle = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.lblTotalBalance = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.btnBillingForm = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.panel5.SuspendLayout();
+			this.panel4.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.Color.Coral;
+			this.panel5.Controls.Add(this.lblBankProcessAmout);
+			this.panel5.Controls.Add(this.lblVakif);
+			this.panel5.Location = new System.Drawing.Point(706, 61);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(225, 125);
+			this.panel5.TabIndex = 10;
+			// 
+			// lblBankProcessAmout
+			// 
+			this.lblBankProcessAmout.AutoSize = true;
+			this.lblBankProcessAmout.Font = new System.Drawing.Font("Cambria", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblBankProcessAmout.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lblBankProcessAmout.Location = new System.Drawing.Point(16, 43);
+			this.lblBankProcessAmout.Name = "lblBankProcessAmout";
+			this.lblBankProcessAmout.Size = new System.Drawing.Size(127, 47);
+			this.lblBankProcessAmout.TabIndex = 5;
+			this.lblBankProcessAmout.Text = "0.00₺";
+			// 
+			// lblVakif
+			// 
+			this.lblVakif.AutoSize = true;
+			this.lblVakif.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblVakif.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lblVakif.Location = new System.Drawing.Point(20, 11);
+			this.lblVakif.Name = "lblVakif";
+			this.lblVakif.Size = new System.Drawing.Size(135, 19);
+			this.lblVakif.TabIndex = 4;
+			this.lblVakif.Text = "Gelen Son Havale";
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.DarkKhaki;
+			this.panel4.Controls.Add(this.lblBillAmout);
+			this.panel4.Controls.Add(this.lblBillTitle);
+			this.panel4.Location = new System.Drawing.Point(462, 61);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(225, 126);
+			this.panel4.TabIndex = 9;
+			// 
+			// lblBillAmout
+			// 
+			this.lblBillAmout.AutoSize = true;
+			this.lblBillAmout.Font = new System.Drawing.Font("Cambria", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblBillAmout.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lblBillAmout.Location = new System.Drawing.Point(27, 43);
+			this.lblBillAmout.Name = "lblBillAmout";
+			this.lblBillAmout.Size = new System.Drawing.Size(127, 47);
+			this.lblBillAmout.TabIndex = 5;
+			this.lblBillAmout.Text = "0.00₺";
+			// 
+			// lblBillTitle
+			// 
+			this.lblBillTitle.AutoSize = true;
+			this.lblBillTitle.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblBillTitle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lblBillTitle.Location = new System.Drawing.Point(31, 11);
+			this.lblBillTitle.Name = "lblBillTitle";
+			this.lblBillTitle.Size = new System.Drawing.Size(111, 19);
+			this.lblBillTitle.TabIndex = 4;
+			this.lblBillTitle.Text = "Fatura Başlığı";
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.Olive;
+			this.panel3.Controls.Add(this.lblTotalBalance);
+			this.panel3.Controls.Add(this.label2);
+			this.panel3.Location = new System.Drawing.Point(205, 61);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(225, 124);
+			this.panel3.TabIndex = 8;
+			// 
+			// lblTotalBalance
+			// 
+			this.lblTotalBalance.AutoSize = true;
+			this.lblTotalBalance.Font = new System.Drawing.Font("Cambria", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblTotalBalance.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lblTotalBalance.Location = new System.Drawing.Point(35, 48);
+			this.lblTotalBalance.Name = "lblTotalBalance";
+			this.lblTotalBalance.Size = new System.Drawing.Size(127, 47);
+			this.lblTotalBalance.TabIndex = 4;
+			this.lblTotalBalance.Text = "0.00₺";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label2.Location = new System.Drawing.Point(39, 11);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(118, 19);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Toplam Bakiye";
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.SteelBlue;
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Location = new System.Drawing.Point(2, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(930, 53);
+			this.panel2.TabIndex = 7;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label1.Location = new System.Drawing.Point(20, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(108, 19);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Banka Formu";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Gray;
+			this.panel1.Controls.Add(this.button8);
+			this.panel1.Controls.Add(this.button7);
+			this.panel1.Controls.Add(this.button6);
+			this.panel1.Controls.Add(this.button5);
+			this.panel1.Controls.Add(this.btnBillingForm);
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(2, 50);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 467);
+			this.panel1.TabIndex = 6;
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.Transparent;
+			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button8.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button8.Location = new System.Drawing.Point(21, 270);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(174, 31);
+			this.button8.TabIndex = 8;
+			this.button8.Text = "Çıkış Yap";
+			this.button8.UseVisualStyleBackColor = false;
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.Transparent;
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button7.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button7.Location = new System.Drawing.Point(21, 233);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(174, 31);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "Ayarlar";
+			this.button7.UseVisualStyleBackColor = false;
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.Transparent;
+			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button6.Location = new System.Drawing.Point(21, 196);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(174, 31);
+			this.button6.TabIndex = 6;
+			this.button6.Text = "Dashboard";
+			this.button6.UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.Transparent;
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button5.Location = new System.Drawing.Point(21, 159);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(174, 31);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "Banka Hareketleri";
+			this.button5.UseVisualStyleBackColor = false;
+			// 
+			// btnBillingForm
+			// 
+			this.btnBillingForm.BackColor = System.Drawing.Color.Transparent;
+			this.btnBillingForm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnBillingForm.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnBillingForm.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnBillingForm.Location = new System.Drawing.Point(21, 122);
+			this.btnBillingForm.Name = "btnBillingForm";
+			this.btnBillingForm.Size = new System.Drawing.Size(174, 31);
+			this.btnBillingForm.TabIndex = 4;
+			this.btnBillingForm.Text = "Giderler";
+			this.btnBillingForm.UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Transparent;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button3.Location = new System.Drawing.Point(21, 85);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(174, 31);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Faturalar";
+			this.button3.UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Transparent;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button2.Location = new System.Drawing.Point(21, 48);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(174, 31);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Bankalar";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.button1.Location = new System.Drawing.Point(21, 11);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(174, 31);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Kategoriler";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// FrmDashboard
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.ClientSize = new System.Drawing.Size(982, 518);
+			this.Controls.Add(this.panel5);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "FrmDashboard";
+			this.Text = "DashBoard/Genel Bakış Formu";
+			this.Load += new System.EventHandler(this.FrmDashboard_Load);
+			this.panel5.ResumeLayout(false);
+			this.panel5.PerformLayout();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Label lblBankProcessAmout;
+		private System.Windows.Forms.Label lblVakif;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Label lblBillAmout;
+		private System.Windows.Forms.Label lblBillTitle;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label lblTotalBalance;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button btnBillingForm;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
 	}
 }
 
--- MyFinancialCRMProjesi/FrmDashboard.cs
+++ MyFinancialCRMProjesi/FrmDashboard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyFinancialCRMProjesi.Models;
 
 namespace MyFinancialCRMProjesi
 {
@@ -15,6 +17,47 @@ namespace MyFinancialCRMProjesi
 		public FrmDashboard()
 		{
 			InitializeComponent();
+		}
+
+		FinancialCrmDbEntities db=new FinancialCrmDbEntities();
+		int count = 0;
+		private void FrmDashboard_Load(object sender, EventArgs e)
+		{
+			var totalBalance = db.Banks.Sum(x => x.BankBalance);
+			lblTotalBalance.Text = totalBalance.ToString() +"₺";
+
+			var lastBankProcessAmout = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(1).Select(y => y.Amount).FirstOrDefault();
+			lblBankProcessAmout.Text = lastBankProcessAmout.ToString() + "₺";
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			count++;
+			if (count % 4 == 1)
+			{
+				var elektrikFaturasi= db.Bills.Where(x => x.BillTitle == "Elektrik Faturası").Select(y => y.BillAmount).FirstOrDefault();
+				lblBillTitle.Text = "Elektrik Faturası";
+				lblBillAmout.Text=elektrikFaturasi.ToString() + "₺";
+			}
+
+			if (count % 4 == 2)
+			{
+				var dogalgazFaturasi = db.Bills.Where(x => x.BillTitle == "Doğalgaz Faturası").Select(y => y.BillAmount).FirstOrDefault();
+				lblBillTitle.Text = "Doğalgaz Faturası";
+				lblBillAmout.Text = dogalgazFaturasi.ToString() + "₺";
+			}
+			if (count % 4 == 3)
+			{
+				var suFaturasi = db.Bills.Where(x => x.BillTitle == "Su Faturası").Select(y => y.BillAmount).FirstOrDefault();
+				lblBillTitle.Text = "Su Faturası";
+				lblBillAmout.Text = suFaturasi.ToString() + "₺";
+			}
+			if (count % 4 == 0)
+			{
+				var internetFaturasi = db.Bills.Where(x => x.BillTitle == "İnternet Faturası").Select(y => y.BillAmount).FirstOrDefault();
+				lblBillTitle.Text = "İnternet Faturası";
+				lblBillAmout.Text = internetFaturasi.ToString() + "₺";
+			}
 		}
 	}
 }
